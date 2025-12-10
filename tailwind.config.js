444673b416445500b8131e561dc7e49ebad2bc0c
Alan Sharfman <alan@example.com>
2025-12-11 07:37:54 +1100

    Remove old CJS PostCSS file

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+      extend: {
+        // Add your custom colors, fonts, spacing, etc here later
+      },
+    },
+    plugins: [],
+  };
+  
