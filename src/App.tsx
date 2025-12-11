23c1b40b818521fb81a9f67adef25a83edad1570
Alan Sharfman <alan@example.com>
2025-12-11 11:43:08 +1100

    Wire MountainEngine into App and render dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,36 @@
 import React from "react";
-import Mountain from "./components/engine/Mountain"; 
-// or use MountainEngine if you want the canvas spline instead
+import MountainEngine from "./components/engine/MountainEngine";
+
+const DEFAULT_POINTS: number[] = [40, 55, 70, 85, 65, 90, 75];
 
 export default function App() {
   return (
-    <div 
+    <div
       style={{
         width: "100vw",
         height: "100vh",
-        background: "#0d0d0d",
+        background: "#050816",
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        padding: "20px",
+        padding: "24px",
         boxSizing: "border-box",
       }}
     >
-      <div 
+      <div
         style={{
           width: "100%",
           height: "100%",
           borderRadius: "20px",
           overflow: "hidden",
+          border: "1px solid rgba(255,255,255,0.15)",
         }}
       >
-        <Mountain />
+        <MountainEngine
+          dataPoints={DEFAULT_POINTS}
+          activeKPIIndex={3}
+          scenario="base"
+        />
       </div>
     </div>
   );
