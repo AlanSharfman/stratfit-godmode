75de42633b08cfc8d570bfdc1375371c8452ae49
Alan Sharfman <alan@stratfit.ai>
2026-02-08 13:10:17 +1100

    chore: lock dev server to 62000

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 import * as path from "path";
 
 export default defineConfig({
+  server: {
+    host: "127.0.0.1",
+    port: 62000,
+    strictPort: true
+  },
   plugins: [react()],
   resolve: {
     alias: {
