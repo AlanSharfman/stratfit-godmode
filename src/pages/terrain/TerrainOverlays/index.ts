e33d2ce17da17af6ea9c285f91356edfbc50c170
Alan Sharfman <alan@stratfit.ai>
2026-02-22 01:02:08 +1100

    audit: remove 13 dead-code files (App.tsx, EngineDebugPanel, pages/terrain, debug/)

--- src/pages/terrain/TerrainOverlays/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as TerrainOverlayLayer } from "./TerrainOverlayLayer";
-export { default as HeatmapOverlay } from "./HeatmapOverlay";
-export { default as TrajectoryOverlay } from "./TrajectoryOverlay";
-export { default as SensitivityNodes } from "./SensitivityNodes";
-export { default as ConfidenceBandOverlay } from "./ConfidenceBandOverlay";
-export { default as HorizonReferenceLine } from "./HorizonReferenceLine";
-export { default as MountainRegionLabels } from "./MountainRegionLabels";
-export { default as OverlayTooltip } from "./OverlayTooltip";
