e597a7347b4f0cbf195e04850204a39f1d221e54
Alan Sharfman <alan@stratfit.ai>
2026-02-24 00:01:40 +1100

    phase3: initiate scaffold

---
+++ src/pages/initiate/InitiatePage.tsx
@@ -0,0 +1,9 @@
+import InitiatePanel from "./InitiatePanel";
+
+export default function InitiatePage() {
+  return (
+    <div style={{ paddingTop: 72 }}>
+      <InitiatePanel />
+    </div>
+  );
+}
